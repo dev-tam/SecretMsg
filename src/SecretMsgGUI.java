900447f0329c5bc059e45d71d9b50110771326a3
dev-tam <tamjid.mdh@gmail.com>
2017-12-30 20:36:12 +0600

    first commit

---
+++ src/SecretMsgGUI.java
@@ -0,0 +1,12 @@
+import javax.swing.JPanel;
+
+public class SecretMsgGUI extends JPanel {
+	public SecretMsgGUI() {
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
